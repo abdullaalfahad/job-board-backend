01bedd1b3a91366875300056a63221700818dcac
abdullaalfahad <aafahad02@gmail.com>
2025-07-23 02:18:31 +0600

    chore: setup ESLint, Prettier, and Husky for code quality enforcement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -9,14 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("API is running"));
+app.get('/', (req, res) => res.send('API is running'));
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log("MongoDB connected");
+    console.log('MongoDB connected');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .catch((err) => console.error('MongoDB connection error:', err));
