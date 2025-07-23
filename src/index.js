3b4db71d7ad305a208711daddb2102855335032f
abdullaalfahad <aafahad02@gmail.com>
2025-07-24 01:01:12 +0600

    chore: move MongoDB connection to separate config file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { connectDB } from './config/db.js';
+
 import jobRoutes from './modules/job/job.routes.js';
 import authRoutes from './modules/auth/auth.routes.js';
 import applicationRoutes from './modules/application/application.routes.js';
@@ -19,10 +20,6 @@ app.use('/applications', applicationRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error('MongoDB connection error:', err));
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
