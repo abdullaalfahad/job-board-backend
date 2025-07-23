f6fed96d2f2eb1f0c2a11d44859c6e8e748d54dd
abdullaalfahad <aafahad02@gmail.com>
2025-07-24 00:10:56 +0600

    feat(auth): implement auth middleware on createJob

--- src/modules/job/job.routes.js
+++ src/modules/job/job.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createJob, getJobById, getJobs } from './job.handlers.js';
+import { auth } from '../../middlewares/auth.js';
 
 const router = express.Router();
 
 router.get('/', getJobs);
 router.get('/:id', getJobById);
-router.post('/', createJob);
+router.post('/', auth, createJob);
 
 export default router;
